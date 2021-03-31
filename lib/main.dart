4b3b84e42273351931fe4a8a5398ed7f51e3cdfa
菅瑞林（北京） <ruilin.jian@chukong-inc.com>
2021-03-31 18:08:04 +0800

    flutter 路由表 简单控件学习

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
+//TODO 1.导入包。此行代码作用是导入了Material UI组件库。Material (opens new window)是一种标准的移动端和web端的视觉设计语言， Flutter默认提供了一套丰富的Material风格的UI组件。
 import 'package:flutter/material.dart';
 
+//TODO 2.应用入口。
+//与C/C++、Java类似，Flutter 应用中main函数为应用程序的入口。main函数中调用了runApp 方法，它的功能是启动Flutter应用。runApp它接受一个Widget参数，在本示例中它是一个MyApp对象，MyApp()是Flutter应用的根组件。
+//main函数使用了(=>)符号，这是Dart中单行函数或方法的简写
 void main() {
   runApp(MyApp());
 }
 
+//TODO 3.应用结构。
+//MyApp类代表Flutter应用，它继承了 StatelessWidget类，这也就意味着应用本身也是一个widget。
+//
+// 在Flutter中，大多数东西都是widget（后同“组件”或“部件”），包括对齐(alignment)、填充(padding)和布局(layout)等，它们都是以widget的形式提供。
+//
+// Flutter在构建页面时，会调用组件的build方法，widget的主要工作是提供一个build()方法来描述如何构建UI界面（通常是通过组合、拼装其它基础widget）。
+//
+// MaterialApp 是Material库中提供的Flutter APP框架，通过它可以设置应用的名称、主题、语言、首页及路由列表等。MaterialApp也是一个widget。
+//
+// home 为Flutter应用的首页，它也是一个widget。
+//
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -22,10 +37,26 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
+      //注册路由表
+      routes: {
+        "new_page": (context) => NewRoute(),
+        // "/": (context) => MyHomePage(title: 'Flutter Demo Home Page'), //注册首页路由
+        "tip_page": (context) => TipRoute(text: "what is it"),
+      },
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
+
+//MyHomePage 是Flutter应用的首页，它继承自StatefulWidget类，表示它是一个有状态的组件（Stateful widget）。关于Stateful widget我们将在第三章“Widget简介”一节仔细介绍，现在我们只需简单认为有状态的组件（Stateful widget） 和无状态的组件（Stateless widget）有两点不同：
+//
+// Stateful widget可以拥有状态，这些状态在widget生命周期中是可以变的，而Stateless widget是不可变的。
+//
+// Stateful widget至少由两个类组成：
+//
+// 一个StatefulWidget类。
+// 一个 State类； StatefulWidget类本身是不变的，但是State类中持有的状态在widget生命周期中可能会发生变化。
+// _MyHomePageState类是MyHomePage类对应的状态类。看到这里，读者可能已经发现：和MyApp 类不同， MyHomePage类中并没有build方法，取而代之的是，build方法被挪到了_MyHomePageState方法中，至于为什么这么做，先留个疑问，在分析完完整代码后再来解答。
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -44,6 +75,16 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
+
+//State类
+// 接下来，我们看看_MyHomePageState中都包含哪些东西：
+//
+// 1.该组件的状态。由于我们只需要维护一个点击次数计数器，所以定义一个_counter状态：
+//  _counter 为保存屏幕右下角带“+”号按钮点击次数的状态。
+// 2.设置状态的自增函数。
+//  当按钮点击时，会调用此函数，该函数的作用是先自增_counter，然后调用setState 方法。setState方法的作用是通知Flutter框架，有状态发生了改变，Flutter框架收到通知后，会执行build方法来根据新的状态重新构建界面， Flutter 对此方法做了优化，使重新执行变的很快，所以你可以重新构建任何需要更新的东西，而无需分别去修改各个widget。
+// 3.构建UI界面
+// 构建UI界面的逻辑在build方法中，当MyHomePage第一次创建时，_MyHomePageState类会被创建，当初始化完成后，Flutter框架会调用Widget的build方法来构建widget树，最终将widget树渲染到设备屏幕上。所以，我们看看_MyHomePageState的build方法中都干了什么事：
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
@@ -67,6 +108,10 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+
+    // Scaffold 是 Material 库中提供的页面脚手架，它提供了默认的导航栏、标题和包含主屏幕widget树（后同“组件树”或“部件树”）的body属性，组件树可以很复杂。本书后面示例中，路由默认都是通过Scaffold创建。
+    // body的组件树中包含了一个Center 组件，Center 可以将其子组件树对齐到屏幕中心。此例中， Center 子组件是一个Column 组件，Column的作用是将其所有子组件沿屏幕垂直方向依次排列； 此例中Column子组件是两个 Text，第一个Text 显示固定文本 “You have pushed the button this many times:”，第二个Text 显示_counter状态的数值。
+    // floatingActionButton是页面右下角的带“+”的悬浮按钮，它的onPressed属性接受一个回调函数，代表它被点击后的处理器，本例中直接将_incrementCounter方法作为其处理函数。
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -100,6 +145,32 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            TextButton(
+                onPressed: () {
+                  //使用路由名打开路由页
+                  Navigator.pushNamed(context, "new_page");
+                  // Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  //   return NewRoute();
+                  // }));
+                },
+                style: TextButton.styleFrom(
+                  textStyle: TextStyle(fontSize: 20),
+                ),
+                child: Text("open new router")),
+            TextButton(
+              onPressed: () async {
+                //打开 TipRoute ，并等待返回结果
+                //使用路由名打开路由页
+                var result3 = await Navigator.pushNamed(context, "tip_page");
+                // var result = await Navigator.push(context,
+                //     MaterialPageRoute(builder: (context) {
+                //   return TipRoute(text: "我是提示XXXX");
+                // }));
+                //输出`TipRoute`路由返回结果
+                print("路由返回值: $result3");
+              },
+              child: Text("打开提示页"),
+            ),
           ],
         ),
       ),
@@ -108,6 +179,55 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+//创建一个新的路由
+class NewRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New Route"),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            Navigator.pop(context, 1);
+          },
+          child: Text("This is new Route"),
+        ),
+      ),
+    );
+  }
+}
+
+class TipRoute extends StatelessWidget {
+  TipRoute({Key key, @required this.text}) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text("提示"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text(text),
+              RaisedButton(
+                onPressed: () => Navigator.pop(context, "我是返回值"),
+                child: Text("返回"),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
